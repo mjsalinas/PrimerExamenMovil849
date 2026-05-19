c9ffc2f714f79e8ad85b68ecfe72b68825fad014
Axl-Ramses <ramses.coto1@gmail.com>
2026-05-18 20:50:22 -0600

    AddActivity
    Se corrigio AddActivityTab - Se corrigió onPress de () => {} a handleSave para ejecutar realmente la función
    de guardado, se invirtió lógica de disabled de isValid a !isValid permitiendo guardar solo cuando
    el formulario contiene datos válidos, se aseguró que la función de guardado agrega la actividad al contexto.

--- PetCareMini/src/screens/AddActivityTab.tsx
+++ PetCareMini/src/screens/AddActivityTab.tsx
@@ -87,8 +87,8 @@ export default function AddActivityTab({ navigation }: any) {
         <View style={styles.buttonContainer}>
           <CustomButton
             title="Guardar Actividad"
-            onPress={()=>{}}
-            disabled={isValid}
+            onPress={handleSave}
+            disabled={!isValid}
             variant="primary"
           />
         </View>
