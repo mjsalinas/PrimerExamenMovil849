fd1c23cd3dd8c1d93f3bad24f518e51316b2e5f1
Robert Taylor <robert.taylor.mk@gmail.com>
2026-05-18 21:01:06 -0600

    -Agregando el handle save al botton para guardar actividad y poniendo ! en isvalid para que solo se envie cuando es valido

--- PetCareMini/src/screens/AddActivityTab.tsx
+++ PetCareMini/src/screens/AddActivityTab.tsx
@@ -88,7 +88,7 @@ export default function AddActivityTab({ navigation }: any) {
           <CustomButton
             title="Guardar Actividad"
             onPress={()=>{}}
-            disabled={isValid}
+            disabled={!isValid}
             variant="primary"
           />
         </View>
