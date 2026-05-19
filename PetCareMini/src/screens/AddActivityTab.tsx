82ca6d78e47fd87acfaa10ca6c24bfb82f35aa4f
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-05-18 20:55:51 -0600

    addactivitytab: agreue el alert que faltaba en la parte de confirmacion , puse el ! en el parametro para que se desabilite el boton cuando no es valido , tambien puse la funcion handlesave dentro del onpress del boton ya que solo habia una funcion vacia

--- PetCareMini/src/screens/AddActivityTab.tsx
+++ PetCareMini/src/screens/AddActivityTab.tsx
@@ -56,6 +56,7 @@ export default function AddActivityTab({ navigation }: any) {
     setSubmitted(false);
 
     // Confirmación
+    Alert.alert('Éxito', 'La actividad se registró correctamente.');
   };
 
   return (
@@ -87,8 +88,8 @@ export default function AddActivityTab({ navigation }: any) {
         <View style={styles.buttonContainer}>
           <CustomButton
             title="Guardar Actividad"
-            onPress={()=>{}}
-            disabled={isValid}
+            onPress={handleSave}
+            disabled={!isValid}
             variant="primary"
           />
         </View>
